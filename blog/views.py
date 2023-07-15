9e664c5177f92113a6a46f0478a0bf3947ed85a7
Riddhub <unkownrid@gmail.com>
2023-07-15 18:40:32 +0300

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,21 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+
+def post_list(request):
+    post_list = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    return render(request, 'post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'post/detail.html', {'post': post})
