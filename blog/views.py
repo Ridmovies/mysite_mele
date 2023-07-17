6123b05ef7ad5f3711078a0b375a3391db616079
Riddhub <unkownrid@gmail.com>
2023-07-17 11:20:51 +0300

    - add ListView class

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -8,8 +11,20 @@ def post_list(request):
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'post/list.html', {'posts': posts})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -19,3 +34,18 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+        # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+        # ... отправить электронное письмо
+    else:
+        form = EmailPostForm()
+    return render(request, 'post/share.html', {'post': post, 'form': form})
